80e34c144c9fbffa7285495fe215a1e4f6201fbf
Jeff Brown Tech <jeff@jeffbrown.tech>
2021-05-13 14:31:12 -0700

    characterpicturegrid

---
+++ AutomateTheBoringStuff/Chapter4/CharacterPictureGrid.py
@@ -0,0 +1,26 @@
+# Character Picture Grid
+# Use the grid variable to print a heart-shaped image, like this:
+# ..OO.OO..
+# .OOOOOOO.
+# .OOOOOOO.
+# ..OOOOO..
+# ...OOO...
+# ....O....
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+# Making assumption that each y length is the same through the grid
+yLen = len(grid[0])
+
+for y in range(yLen):
+    for x in range(len(grid)):
+        print(grid[x][y], end='')
+    print('')
