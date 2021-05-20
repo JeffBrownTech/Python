c25050a45068a47131d91e69f22de984221ab2e5
Jeff Brown Tech <jeff@jeffbrown.tech>
2021-05-20 12:23:00 -0700

    multi player functionality

--- AutomateTheBoringStuff/Chapter5/TicTacToe.py
+++ AutomateTheBoringStuff/Chapter5/TicTacToe.py
@@ -1,3 +1,6 @@
+import random
+import time
+
 def printBoard(board):
     print('\n' + board['1'] + '|' + board['2'] + '|' + board['3'])
     print('-|-|-')
@@ -5,15 +8,22 @@ def printBoard(board):
     print('-|-|-')
     print(board['7'] + '|' + board['8'] + '|' + board['9'])
 
+def computerMove():
+    print('\nThe computer is making a move...')
+    time.sleep(random.randint(1,5))
+    index = random.randint(0, len(openMoves) -1)
+    print('\nThe computer chose space ' + openMoves[index] + '.')
+    return openMoves[index]
+
 def checkWinner(board, letter):
     return((board['1'] == letter and board['2'] == letter and board['3'] == letter) or # across the top
-           (board['3'] == letter and board['4'] == letter and board['5'] == letter) or # across the middle
-           (board['4'] == letter and board['5'] == letter and board['6'] == letter) or # across the bottom
-           (board['1'] == letter and board['4'] == letter and board['5'] == letter) or # down the left
+           (board['4'] == letter and board['5'] == letter and board['6'] == letter) or # across the middle
+           (board['7'] == letter and board['8'] == letter and board['9'] == letter) or # across the bottom
+           (board['1'] == letter and board['4'] == letter and board['7'] == letter) or # down the left
            (board['2'] == letter and board['5'] == letter and board['8'] == letter) or # down the middle
            (board['3'] == letter and board['6'] == letter and board['9'] == letter) or # down the right
            (board['1'] == letter and board['5'] == letter and board['9'] == letter) or # diagonal
-           (board['5'] == letter and board['5'] == letter and board['7'] == letter) # diagonal
+           (board['3'] == letter and board['5'] == letter and board['7'] == letter) # diagonal
     )
 
 theBoard = {'1': '1', '2': '2', '3': '3',
@@ -21,28 +31,56 @@ theBoard = {'1': '1', '2': '2', '3': '3',
             '7': '7', '8': '8', '9': '9'}
 
 openMoves = ['1','2','3','4','5','6','7','8','9']
-turn = 'X'
 
-for i in range(9):
+print('***** Welcome to Tic-Tac-Toe! *****')
+
+while True:
+    players = input('1 or 2 players? ')
+    if (players != '1') and (players != '2'):
+        print('Please enter 1 or 2 to select the number of players.')
+    else:
+        break
+
+turn = 'X'
+player1Name = input('Enter Player 1 Name: ')
+player2Name = None
+currentPlayer = player1Name
+if players == '1':
+    player2Name = 'computer'
+else:
+    player2Name = input('Enter Player 2 Name: ')
+
+for n in range(9):
     printBoard(theBoard)
-        
-    while True:
-        move = input('\nTurn for ' + turn + '. Move on which space: ')
-        if move in openMoves:
-            openMoves.remove(move)
-            break
-        else:
-            print('That space is already taken. Please try again.')
+    
+    if currentPlayer == 'computer':
+        move = computerMove()
+        openMoves.remove(move)
+    else:
+        while True:
+            move = input('\nTurn for ' + currentPlayer + '. Move on which space: ')
+            if move in openMoves:
+                openMoves.remove(move)
+                break
+            else:
+                print('That space is already taken. Please try again.')
     
     theBoard[move] = turn
 
     if checkWinner(theBoard, turn):
-        print('Player ' + turn + ' has won the game!')
+        if currentPlayer == 'computer':
+            print('Oh man! You let the computer beat you!')
+        else:
+            print('\nPlayer ' + currentPlayer + ' has won the game!')
         break
     else:
-        if turn == 'X':
+        if n == 8:
+            print('\nNo one won! It\'s a draw!')
+        elif turn == 'X':
+            currentPlayer = player2Name
             turn = 'O'
         else:
+            currentPlayer = player1Name
             turn = 'X'
 
 printBoard(theBoard)
